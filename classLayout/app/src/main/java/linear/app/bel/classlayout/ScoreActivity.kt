4f0b2306c9599189a91e44d354aeca2bfe3c9b1b
Unknown <feetbonike90@gmail.com>
2018-03-05 15:36:20 +0700

    tambahan team 2

--- classLayout/app/src/main/java/linear/app/bel/classlayout/ScoreActivity.kt
+++ classLayout/app/src/main/java/linear/app/bel/classlayout/ScoreActivity.kt
@@ -6,19 +6,45 @@ package linear.app.bel.classlayout
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.score.*
 
 
 class ScoreActivity : AppCompatActivity() {
 
     var jumlah = 0
+    var jumlah2 = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.score)
 
-        btn.setOnClickListener {
+        btnScore1.setOnClickListener {
             jumlah = jumlah + 1
-            text.text = "$jumlah"
+            nilaiTeam1.text = "$jumlah"
+        }
+
+        btnScore2.setOnClickListener {
+            jumlah = jumlah + 2
+            nilaiTeam1.text = "$jumlah"
+        }
+
+        btnScore3.setOnClickListener {
+            jumlah = jumlah + 3
+            nilaiTeam1.text = "$jumlah"
+        }
+
+
+        btnScore1Team2.setOnClickListener {
+            jumlah2 = jumlah2 + 1
+            nilaiTeam2.text = "$jumlah2"
+        }
+
+        btnScore2Team2.setOnClickListener {
+            jumlah2 = jumlah2 + 2
+            nilaiTeam2.text = "$jumlah2"
+        }
+        btnScore3Team2.setOnClickListener {
+            jumlah2 = jumlah2 + 3
+            nilaiTeam2.text = "$jumlah2"
         }
 
     }
