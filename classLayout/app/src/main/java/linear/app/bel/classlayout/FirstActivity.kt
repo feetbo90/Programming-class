0e2def4ddfdf60de1a5670b3188a21d654c008f1
Unknown <feetbonike90@gmail.com>
2018-03-05 16:35:31 +0700

    commit terbaru

--- classLayout/app/src/main/java/linear/app/bel/classlayout/FirstActivity.kt
+++ classLayout/app/src/main/java/linear/app/bel/classlayout/FirstActivity.kt
@@ -3,6 +3,8 @@ package linear.app.bel.classlayout
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_first.*
+
 
 class FirstActivity : AppCompatActivity() {
 
@@ -11,9 +13,11 @@ class FirstActivity : AppCompatActivity() {
         setContentView(R.layout.activity_first)
 
 
-        val intent = Intent(this@FirstActivity, SecondActivity::class.java)
-        intent.putExtra("nilai", "1")
-        startActivity(intent)
+        btn.setOnClickListener {
+            val intent = Intent(this@FirstActivity, SecondActivity::class.java)
+            intent.putExtra("nilai2", "1")
+            startActivity(intent)
+        }
 
 
     }
