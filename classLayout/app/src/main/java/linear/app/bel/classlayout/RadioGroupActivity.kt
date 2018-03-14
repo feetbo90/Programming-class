cf9fe18486139394b0efcd8feb0cddf98cfe7941
Unknown <feetbonike90@gmail.com>
2018-03-14 15:33:59 +0700

    radio group

---
+++ classLayout/app/src/main/java/linear/app/bel/classlayout/RadioGroupActivity.kt
@@ -0,0 +1,34 @@
+package linear.app.bel.classlayout
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_radio_group.*
+
+class RadioGroupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_radio_group)
+
+        btn.setOnClickListener {
+
+            if(lk.isChecked){
+
+
+                Toast.makeText(this@RadioGroupActivity, "Laki - Laki", Toast.LENGTH_LONG).show()
+
+            }else if(pr.isChecked)
+                Toast.makeText(this@RadioGroupActivity, "Perempuan ", Toast.LENGTH_LONG).show()
+
+
+            if(lk2.isChecked)
+                Toast.makeText(this@RadioGroupActivity, "Laki - Laki ", Toast.LENGTH_LONG).show()
+
+        }
+
+
+    }
+}
